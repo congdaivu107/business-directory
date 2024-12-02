762d753a758b427c47e2e7889813f2f6aa2c7c16
congdaivu107 <congdaivu107@gmail.com>
2024-12-03 02:50:43 +0700

    update Home Screen ( Business List Display)

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,16 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import Header from './../../components/Home/Header';
+import Slider from '../../components/Home/Slider';
+import Category from '../../components/Home/Category';
+import PopularBusiness from '../../components/Home/PopularBusiness';
 export default function home() {
   return (
-    <View>
+    <ScrollView>
       {/* Header */}
         <Header/>
       {/* Slider */}
-
+        <Slider/>
       {/* Category */}
-      
+        <Category/>
       {/* Popular Business List  */}
-     </View>
+        <PopularBusiness/>
+        <View style={{ height:50}}></View>
+     </ScrollView>
   )
 }
