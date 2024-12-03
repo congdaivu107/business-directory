72d1d4a5c16ca422d7c42742f33244954c986fa6
congdaivu107 <congdaivu107@gmail.com>
2024-12-03 09:55:35 +0700

    Business Review Section

--- app/businessdetail/[businessid].jsx
+++ app/businessdetail/[businessid].jsx
@@ -7,6 +7,7 @@ import { Colors } from '../../constants/Colors';
 import Intro from '../../components/BusinessDetail/Intro';
 import ActionButton from '../../components/BusinessDetail/ActionButton';
 import About from '../../components/BusinessDetail/About';
+import Review from '../../components/BusinessDetail/Review';
 
 export default function BusinessDetail() {
 
@@ -22,7 +23,7 @@ export default function BusinessDetail() {
         const docSnap = await getDoc(docRef);
         if( DocumentSnapshot.exists()){
             console.log("Document data:", docSnap.data());
-            setBusiness(docSnap.data());
+            setBusiness({id:docSnap.id,...docSnap.data()});
             setLoading(false);
         }else{
             console.log("No much document");
@@ -46,6 +47,8 @@ export default function BusinessDetail() {
             <ActionButton business={business}/>
             {/* About Section  */}
             <About business={business}/>
+            {/* Review Section  */}
+            <Review business={business}/>
         </View>    
     }
     </ScrollView>
